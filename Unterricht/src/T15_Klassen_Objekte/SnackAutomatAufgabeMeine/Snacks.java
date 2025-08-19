091e3fa813755d1d3676645cfffd6bb553f4841d
Maksim <maksim.scheierman@gmx.de>
2025-08-19 10:58:34 +0200

    Aufgabe 2 --in work

--- Unterricht/src/T15_Klassen_Objekte/SnackAutomatAufgabeMeine/Snacks.java
+++ Unterricht/src/T15_Klassen_Objekte/SnackAutomatAufgabeMeine/Snacks.java
@@ -9,10 +9,11 @@ public class Snacks {
     //Variables
     private String name;
     private int amount;
+    private double price;
     private Map<String, Double> nutrients = new HashMap<>();
 
     //Constructor
-    public Snacks(String name, int amount, double fat, double carbs, double protein){
+    public Snacks(String name, int amount, double price, double fat, double carbs, double protein){
         this.name = name;
         this.amount = amount;
 
@@ -23,13 +24,14 @@ public class Snacks {
 
     //Methods
     public static void createInitialSnacks(ArrayList<Snacks> snacks){
-        snacks.add(new Snacks("Chips",8,35.0,50.0,6.0));
-        snacks.add(new Snacks("Chocolate",5,30.0,55.0,5.0));
-        snacks.add(new Snacks("Nuts",1,50.0,20.0,25.0));
+        snacks.add(new Snacks("Chips",8,2.10,35.0,50.0,6.0));
+        snacks.add(new Snacks("Chocolate",5, 2.50,30.0,55.0,5.0));
+        snacks.add(new Snacks("Nuts",1, 3.0,50.0,20.0,25.0));
 
     }
     public static void showSnacks(ArrayList<Snacks> snacks){
         for (Snacks snack : snacks){
+            //System.out.println("Name " + snack.name + " Menge " + snack.amount + " Nährwerte " + snack.nutrients);
             System.out.println("Name " + snack.name + " Menge " + snack.amount + " Nährwerte " + snack.nutrients);
         }
     }
