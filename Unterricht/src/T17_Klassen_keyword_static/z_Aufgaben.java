10a899c299c045c3c8ba237c2c5fa39bb6bf0db1
Semih Ibicioglu <semih.ibicioglu@smartindustrycampus.de>
2025-08-27 10:08:22 +0200

    Static hinzugefügt

---
+++ Unterricht/src/T17_Klassen_keyword_static/z_Aufgaben.java
@@ -0,0 +1,79 @@
+package T17_Klassen_keyword_static;
+
+public class z_Aufgaben {
+
+
+
+    /* Übungsaufgaben */
+    /*
+    1. Autofabrik
+        - Erstelle eine Klasse "Auto" mit den Atributen Marke, Modell
+        - Sie soll eine Variable enthalten die Zählt wie viele Autos erstellt worden sind.
+        - Lege einen Fuhrpark mit Autos an, lasse dir ausgeben wie viele Autos insgesamt gebaut wurden.
+
+    2. Zinzfaktor für alle Konten
+        - Es soll eine Klasse Konto mit Kontonummer und Kontostand geben.
+        - Es soll einen Zinssatz geben der für alle Konten immer gleich ist.
+        - Lege mehrere Konten mit unterschiedlichem Kontostand an.
+        - Lege einen Zinssatz fest, verrechne den Zinssatz mit dem Kontostand.
+        -> Gib den neuen Kontostand aus.
+
+     3. Für ein Fitnessstudio sollen Nutzerkonten mit einer Mitgliedsnummer (ID) angelegt werden.
+        - Jeder Nutzer erhält vom System automatisch eine ID.
+        - Die ID soll immer um 1 hochgezählt werden.
+        - Die Vertragsart (z.B: Basic oder Premium) soll gespeichert werden.
+        - Angestellte sollen folgendes machen können:
+            - Mitglieder hinzufügen
+            - Mitglieder löschen
+            - ALle Mitglieder anzeigen
+            - Einen Bestimmten Mitgliedsnamen suchen.
+
+
+     */
+
+
+
+    /**                                     Übung
+     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+     *  Übung für static Methoden (Util Klassen)
+     *
+     *  1. Erstelle eine Klasse "Calculator"
+     *  2. Erstelle je eine Methode für :
+     *      - addition
+     *      - subtraktion
+     *      - division
+     *      - mulitplikation
+     *  3. Die Methoden sollen jeweils zwei Werte über die Parameterliste akzeptieren.
+     *  4. das Ergebnis soll via return zurückgegeben werden.
+     *  5. Rufe in einer _anderen_ Klasse eine Main-Methode auf.
+     *  6. Greife in der Main Methode auf die erstellten Methoden aus "Calculator" zurück OHNE
+     *      Objekte aus der "Calculator"-Klasse zu instanziieren.
+     *  7. Lasse dir die Ergebnisse der Methoden auf der Konsole ausgeben.
+     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+     */
+
+
+
+
+
+
+
+    /************************* ALTE AUFGABEN ***************/
+    /***********************************************************************************************************************************/
+
+
+    /**                                     AUFGABE
+     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+     *  1. Lasse dir eine zufällige Zahl zwischen 100 und 200 generieren.
+     *  2. Lege einen VW Fuhrpark an, der genauso groß ist, wie die zuvor generierte zufällige Zahl.
+     *  3. Die Seriennummer soll Beginnen bei SN1000 und mit jedem Auto um 1 hochzählen:
+     *          SN1000, SN1001, SN1002, SN1003, SN1004, etc.
+     *  4. Konsolenausgabe
+     *      a) Gib auf der Konsole aus wie viele Objekte vom Typ Auto erstellt wurden.
+     *      b) Gib auf der Konsole aus wie viele Autos im Fuhrpark sind.
+     *  5. Nutze die Konsole, um basierend auf der Seriennummer ein Auto aus dem Fuhrpark zu entfernen.
+     *      a) Gib aus wie viele Objekte vom Typ Auto erstellt wurden.
+     *      b) Gib aus wie viele Autos im Fuhrpark sind.
+     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+     */
+}
