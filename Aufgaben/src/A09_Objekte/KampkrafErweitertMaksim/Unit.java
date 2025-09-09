f8414a8ff640984b9a2631ae63f4c8c681c696e4
Maksim <maksim.scheierman@gmx.de>
2025-09-09 12:44:50 +0200

    Neuen Klassen UnitFactory Dice entsprechende Anpassungen anderer Klassen

--- Aufgaben/src/A09_Objekte/KampkrafErweitertMaksim/Unit.java
+++ Aufgaben/src/A09_Objekte/KampkrafErweitertMaksim/Unit.java
@@ -2,13 +2,13 @@ package A09_Objekte.KampkrafErweitertMaksim;
 
 public class Unit {
     //Variables
-    String name;
-    Integer power;
-    Integer cost;
-    Integer lifepoints;
+    private String name;
+    private Integer cost;
+    private Integer lifepoints;
+    private Dice power;
 
     //Constructor
-    public Unit ( String name, Integer power, Integer cost, Integer lifepoints){
+    public Unit ( String name,Integer cost,  Integer lifepoints, Dice power){
         this.name = name;
         this.power = power;
         this.cost = cost;
@@ -17,27 +17,12 @@ public class Unit {
 
     //Getter & Setter
     public String getName(){return this.name;}
-    public Integer getPower(){return this.power;}
+    public String getPower(){return power.toString();}
+    public Integer getRoll(){return power.roll();}
     public Integer getCost(){return this.cost;}
     public Integer getLifepoints(){return this.lifepoints;}
-
-    public void setPower(int power){
-        if(power > 0){
-            this.power = power;
-        }
+    public Dice getDice() {
+        return power;
     }
 
-    //Methods
-    public int calculateDamage(Unit opponent) {
-        return this.getPower(); // Basisschaden
-    }
-
-    @Override
-    public String toString() {
-        // Die Ausgabe des Typs erfolgt jetzt über den Klassennamen
-        return String.format("Einheit: %s (%s) | Stärke: %d, Leben: %d, Kosten: %d",
-                this.name, this.getClass().getSimpleName(), this.power, this.lifepoints, this.cost);
-    }
 }
-
-
