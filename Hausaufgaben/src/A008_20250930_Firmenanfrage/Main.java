74f9cbf09673e9de5f9db619dbf55009b75e6294
Maksim <maksim.scheierman@gmx.de>
2025-10-01 11:43:38 +0200

    Hausaufgabe Abstrakte Klasse, Nachrichten versenden

--- Hausaufgaben/src/A008_20250930_Firmenanfrage/Main.java
+++ Hausaufgaben/src/A008_20250930_Firmenanfrage/Main.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Nachricht> nachrichten = new ArrayList<>();
+
         // 4 Emails, 3 SMS, 5 Briefe
         nachrichten.addAll(NachrichtenFactory.erstelleEmails(4));
         nachrichten.addAll(NachrichtenFactory.erstelleSMS(3));
         nachrichten.addAll(NachrichtenFactory.erstelleBriefe(5));
+
         System.out.println();
         for (Nachricht n : nachrichten) {
             long dauer = n.send();
