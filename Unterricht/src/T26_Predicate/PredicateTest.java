743e92a1ece20b6cf106f35c143f206eb2706c50
TaeOh24 <anita.tomme@smartindustrycampus.de>
2025-10-20 12:13:07 +0200

    PredicateTest zum Ausprobieren von Predicate Funktionen

--- Unterricht/src/T26_Predicate/PredicateTest.java
+++ Unterricht/src/T26_Predicate/PredicateTest.java
@@ -1,5 +1,6 @@
 package T26_Predicate;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 /**
@@ -20,5 +21,26 @@ import java.util.function.Predicate;
 
 public class PredicateTest {
     public static void main(String[] args) {
+        //Predicate<Integer> isEven = number -> number % 2 == 0;
+
+        /*Predicate<Integer> isEvenExpanded = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer number) {
+                return number % 2 == 0;
+            }
+        };*/
+        //System.out.println(isEven.test(25));
+
+
+        //Predicate<Integer> isBiggerThan20 = x -> x > 20;
+        //System.out.println(isBiggerThan20.test(19));
+
+        //Predicate<String> beginWithA = str -> str != null && str.toLowerCase().startsWith("a");
+        //System.out.println(beginWithA.test(null));
+
+        BiPredicate<String, Integer> lengthMatches = (str, i) -> str!=null && str.length()==i;
+        System.out.println(lengthMatches.test("Hallo", 6));
+
     }
+
 }
