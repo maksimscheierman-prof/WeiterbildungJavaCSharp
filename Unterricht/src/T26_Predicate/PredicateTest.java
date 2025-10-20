9a18195ce74582e1f2d5ae08b0f314a0c8dae422
TaeOh24 <anita.tomme@smartindustrycampus.de>
2025-10-20 10:47:41 +0200

    PredicateTest zum Ausprobieren von Predicate Funktionen

---
+++ Unterricht/src/T26_Predicate/PredicateTest.java
@@ -0,0 +1,24 @@
+package T26_Predicate;
+
+import java.util.function.Predicate;
+
+/**
+ * Aufbau des funktionalen Interfaces Predicate:
+ *
+ * @FunctionalInterface
+ * public interface Predicate<T> {
+ *
+ *      Evaluates this predicate on the given argument.
+ *
+ *      *@param t the input argument
+ *      *@return {@code true} if the input argument matches the predicate,
+ *      otherwise {@code false}
+ *
+ *      boolean test(T t);
+ * }
+ */
+
+public class PredicateTest {
+    public static void main(String[] args) {
+    }
+}
