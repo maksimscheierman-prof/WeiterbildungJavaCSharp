5e03f0058ccf32ee7417f2cc23113df4d0390500
Maksim <maksim.scheierman@gmx.de>
2025-10-22 10:12:40 +0200

    Fehler Anpassungen

--- Aufgaben/src/Test/Test.java
+++ Aufgaben/src/Test/Test.java
@@ -1,18 +1,22 @@
 package Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 class Test {
     public static void main(String[] args) {
-   while(true)
-   {
-       int i = 0;
-       Random r = new Random();
-       i= r.nextInt(10);
-       System.out.println(i);
+        // Beispiel 1: Liste von Strings
+        List<String> words = new ArrayList<>(Arrays.asList("Apfel", "Banane", "Kiwi", "", "Birne", "Orange", ""));
+        System.out.println("Vor removeIf: " + words);
 
+        // Entfernt alle leeren Strings
+        words.removeIf(x -> x.isEmpty());
+        System.out.println("Nach removeIf(x -> x.isEmpty()): " + words);
 
-
-   }
+        // Entfernt alle Strings, deren Länge > 5 ist
+        words.removeIf(x -> x.length() > 5);
+        System.out.println("Nach removeIf(x -> x.length() > 5): " + words);
     }
 }
