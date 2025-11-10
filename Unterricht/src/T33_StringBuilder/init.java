e1619b339aa7d4523fc849cd7bd8a64f3206d57a
TaeOh24 <anita.tomme@smartindustrycampus.de>
2025-11-10 13:59:38 +0100

    StringBuilder Code-Beispiele

---
+++ Unterricht/src/T33_StringBuilder/init.java
@@ -0,0 +1,26 @@
+package T33_StringBuilder;
+
+public class init {
+    public static void main(String[] args) {
+        StringBuilder b = new StringBuilder("Hallo");
+        StringBuilder c = b.append(" mein Name ist");
+        b = b.append(" Anita").append(" !");
+        //System.out.println(b);
+        //System.out.println(c);
+        //System.out.println(b.equals(c));
+
+        StringBuilder d = new StringBuilder("Hallo mein Name ist Anita !");
+        System.out.println(b.equals(d));
+
+
+        /*c = c.append("Ich ändere mich nochmal");
+        System.out.println(b);
+        System.out.println(c);
+
+        StringBuilder d = new StringBuilder(c);
+        d.append("Abc");
+        System.out.println(c);
+        System.out.println(d);*/
+    }
+
+}
